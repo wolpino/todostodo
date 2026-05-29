e1c3bca95a069f820a7e06df23bb0bb6a337eb4d
wolpino <ariwolpin@gmail.com>
2026-05-29 08:08:09 -0700

    init sqllite db

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -10,14 +10,9 @@ public class ApplicationDbContext: IdentityDbContext<ApplicationUser, IdentityRo
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
-        
+       
     }
         
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        base.OnConfiguring(optionsBuilder);
-    }
-
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
